7b3f3c009a89585e6f63cba3b1daa87387a9d6fd
ZISQO <50421769+ZISQO@users.noreply.github.com>
2019-11-27 22:51:55 +0900

    Update and rename boot-macOS-Catalina.sh to boot-catalina.sh

--- boot-catalina.sh
+++ boot-catalina.sh
@@ -29,7 +29,7 @@ qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -device ide-hd,bus=sata.2,drive=Clover \
 	  -device ide-hd,bus=sata.3,drive=InstallMedia \
 	  -drive id=InstallMedia,if=none,file=BaseSystem.img,format=raw \
-	  -drive id=MacHDD,if=none,file=./mac_hdd_ng.img,format=qcow2 \
+	  -drive id=MacHDD,if=none,file=./macOS_hdd.img,format=qcow2 \
 	  -device ide-hd,bus=sata.4,drive=MacHDD \
 	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
 	  -monitor stdio \
