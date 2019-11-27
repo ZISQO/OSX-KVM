dad05c86e656a4ffecdb733e1d19264d3bc4ded8
ZISQO <50421769+ZISQO@users.noreply.github.com>
2019-11-28 03:47:36 +0900

    Update boot-catalina.sh

--- boot-catalina.sh
+++ boot-catalina.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# qemu-img create -f qcow2 mac_hdd_ng.img 128G
+# qemu-img create -f qcow2 macOS_hdd.img 128G
 #
-# echo 1 > /sys/module/kvm/parameters/ignore_msrs (this is required)
+# echo 1 > /sys/module/kvm/parameters/ignore_msrs (이 작업은 필수입니다)
 
 ############################################################################
-# NOTE: Tweak the "MY_OPTIONS" line in case you are having booting problems!
+# NOTE: "MY_OPTIONS" 값을 변경하면 부팅 오류를 해결 할 수 있습니다                    #
 ############################################################################
 
 # This works for Catalina as well as Mojave. Tested with macOS 10.14.6 and macOS 10.15.
@@ -34,3 +34,6 @@ qemu-system-x86_64 -enable-kvm -m 3072 -cpu Penryn,kvm=on,vendor=GenuineIntel,+i
 	  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -device vmxnet3,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
 	  -monitor stdio \
 	  -vga vmware
+
+# 이 설치는 QEMU 윈도우를 이용해 직접 설치할 때 사용하며 클린 설치 이외엔 실행할 일이 없습니다
+# macOS.xml을 필요에 맞게 변경해 운영체제를 설정을 마감하면 됩니다
